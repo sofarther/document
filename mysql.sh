0bd50cf8c80929a6613573a2e308a9af486c9de6
sofarther <794630531@qq.com>
2017-09-05 17:32:06 +0800

    mongodb php 注意事项

--- mysql.sh
+++ mysql.sh
@@ -7,13 +7,13 @@ mysql_cmd="MYSQL remote -u root"
 #-B 表示批处理模式， -s 禁止输出列标题和格式化符号
 $MYSQL remote -u root -Bse 'show tables'
 
-sql="select * from jwr_access_list  where access_id >666 limit 3;
-	select * from jwr_access_list where access_id <10 limit 2 	
+sql="select * from access_list  where access_id >666 limit 3;
+	select * from access_list where access_id <10 limit 2 	
 "
 #-X 表示返回XML 格式数据
 << XML
 
-<resultset statement="select * from jwr_access_list where access_id &lt;10 limit 2
+<resultset statement="select * from access_list where access_id &lt;10 limit 2
 " xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
   <row>
         <field name="access_id">6</field>
@@ -40,7 +40,7 @@ sql="select * from jwr_access_list  where access_id >666 limit 3;
 XML
 
 $MYSQL remote -u root -X -Bse "$sql"
-#$mysql_cmd  -e "insert into jwr_access_list values(null,'182.168.1.101',100,'http://wd.juwairen.net',158374833,0,0,0)"
+#$mysql_cmd  -e "insert into access_list values(null,'182.168.1.101',100,'http://wd.net',158374833,0,0,0)"
 #-u 和用户，-p和密码 之间必须没有空格 ，否则会进行交互式提示输入密码 
-# jwairen 指定连接的数据库名
-$MYSQL -h 192.168.1.127 jwairen -usofar -psofar -e "insert into jwr_admin values(null,'test','passwd','love email','qq','15334324','192.168.1.102','4212121',1)"
+# mystest 指定连接的数据库名
+$MYSQL -h 192.168.1.127 mystest -usofar -psofar -e "insert into admine values(null,'test','passwd','love email','qq','15334324','192.168.1.102','4212121',1)"
